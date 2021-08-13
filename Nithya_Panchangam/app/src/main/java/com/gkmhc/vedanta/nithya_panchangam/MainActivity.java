dff9d5ff582def7ca452ab5b47390cd7f78689c5
gkmhcreations <gkmhcreations@gmail.com>
2021-08-13 14:07:01 +0530

    Minor corrections.
    Minor corrections.

--- Nithya_Panchangam/app/src/main/java/com/gkmhc/vedanta/nithya_panchangam/MainActivity.java
+++ Nithya_Panchangam/app/src/main/java/com/gkmhc/vedanta/nithya_panchangam/MainActivity.java
@@ -34,7 +34,6 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
 import android.widget.Toast;
 import com.gkmhc.utils.CopyToAssets;
 import com.gkmhc.utils.VedicCalendar;
